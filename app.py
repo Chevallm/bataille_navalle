538300e9fa9bb452312487843dec836b8bb2a16c
Maxime <maxime.chevallier_pichon@yahoo.fr>
2016-12-08 15:23:56 +0100

    v0.1

--- app.py
+++ app.py
@@ -1,46 +1,106 @@
+# Importer le random
 from random import randint
 
+exit = False
+
+# Création du plateau
 plateau = []
 
-nb_ligne_col = 5
-nb_tours = 4
+nb_ligne_col = 10 # Nombre de ligne et de colonnes
+nb_tours = 4 # Nombre de tours
+espace = " " # Caractère espace
 
-for x in range(0, nb_ligne_col):
-    plateau.append(["O"] * nb_ligne_col)
-
-def afficher_plateau(plateau):
-    for ligne in plateau:
-        print " ".join(ligne)
-
-afficher_plateau(plateau)
-
+# Retourne une ligne aléatoire
 def alea_ligne(plateau):
     return randint(0, len(plateau) - 1)
-
+# Retourne une colonne aléatoire
 def alea_col(plateau):
     return randint(0, len(plateau[0]) - 1)
 
-bateau_x = alea_ligne(plateau)
-bateau_y = alea_col(plateau)
-print "Bateau en " + str(bateau_x) + ":" + str(bateau_y)
+# Remplir le tableau de O
+def initPlateau(plateau):
+    for x in range(0, nb_ligne_col):
+        plateau.append(["O"] * nb_ligne_col)
 
-for tour in range(nb_tours):
-    print("Tour n " + str(tour+1))
-    print("- - - - - - -")
-    tir_x = int(raw_input("Quelle ligne ?"))
-    tir_y = int(raw_input("Quelle colonne ?"))
+# Vide le tableau
 
-    if tir_x == bateau_x and tir_y == bateau_y:
-        print "Bravo ! Vous avez eu mon bateau !"
-        break
-    else:
-        if (tir_x < 0 or tir_x > nb_ligne_col-1) or (tir_y < 0 or tir_y > nb_ligne_col-1):
-            print "Oups ! Vous devez viser l'ocean."
-        elif(plateau[tir_x ][tir_y ] == "X"):
-            print "Cette case est deja decouverte."
+
+# Afficher le tableau
+def afficher_plateau(plateau):
+    for ligne in plateau:
+        print(espace.join(ligne)) # Afficher des espace entre chaque cellule
+
+def afficher_le_numero_du_tour():
+    print("Tour N "+ str(tour+1))
+
+def faire_tirer_le_joueur():
+    return {
+        "tx": int(input("Quelle ligne ?"))-1,
+        "ty": int(input("Quelle colonne ?"))-1
+    }
+
+def flatten(lignes_du_plateau):
+    r = []
+    for ligne in lignes_du_plateau:
+        for n in ligne:
+            r.append(n)
+    return r
+
+def vider_le_plateau(plateau):
+    flatten(plateau)
+    for value in plateau:
+        plateau.remove(value)
+    return plateau
+
+def est_fini(nb_tours):
+    return nb_tours > 0
+
+def demander_nombre_de_tour():
+    return int(input("Combien de tours voulez-vous ?"))-1
+
+def afficher_le_titre():
+    print("**************************************************")
+    print("*                     BATAILLE                   *")
+    print("*                      NAVALE                    *")
+    print("**************************************************")
+    print()
+    print("Ecrit par: MCP <maxime.chevallier_pichon@yahoo.fr>")
+    print("Open source 2016")
+    print()
+    print()
+
+afficher_le_titre()
+while(exit == False):
+
+    nb_tours = demander_nombre_de_tour()+1
+
+    vider_le_plateau(plateau)
+    initPlateau(plateau)
+    afficher_plateau(plateau)
+
+    # Placement du bateau aléatoirement
+    bateau_x = alea_ligne(plateau)
+    bateau_y = alea_col(plateau)
+
+    # Affiche la localisation du bateau
+    #print("Bateau en " + str(bateau_x) + ":" + str(bateau_y))
+
+    # Boucle du jeu
+
+    for tour in range(0,nb_tours):
+        afficher_le_numero_du_tour()
+        coordonnees_du_tir = faire_tirer_le_joueur();
+
+        if coordonnees_du_tir["tx"] == bateau_x and coordonnees_du_tir["ty"] == bateau_y:
+            print("Bravo ! Vous avez eu mon bateau !")
+            break
         else:
-            print "Dommage !"
-            plateau[tir_x ][tir_y ] = "X"
-        # Affichez tour+1 ici !
+            if (coordonnees_du_tir["tx"] < 0 or coordonnees_du_tir["tx"] > nb_ligne_col-1) or (coordonnees_du_tir["ty"] < 0 or coordonnees_du_tir["ty"] > nb_ligne_col-1):
+                print("Oups ! Vous devez viser l'ocean.")
+            elif(plateau[coordonnees_du_tir["tx"] ][coordonnees_du_tir["ty"] ] == "X"):
+                print("Cette case est deja decouverte.")
+            else:
+                print("Dans l'eau !")
+                plateau[coordonnees_du_tir["tx"] ][coordonnees_du_tir["ty"] ] = "X"
 
-        afficher_plateau(plateau)
+            print(est_fini(nb_tours))
